427fb21a7655172b27ce48971993152064e38298
Michael O'Brien <michael@obrienlabs.org>
2025-09-23 22:46:08 -0400

    #1 - spring boot template

---
+++ src/main/java/dev/obrienlabs/biometric/BiometricBackendMqttHttp3Application.java
@@ -0,0 +1,13 @@
+package dev.obrienlabs.biometric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiometricBackendMqttHttp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiometricBackendMqttHttp3Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/obrienlabs/biometric/BiometricBackendMqttHttp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.obrienlabs.biometric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiometricBackendMqttHttp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
